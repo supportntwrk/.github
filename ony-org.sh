909557f058ee5f86449e07c979819a4f4c16e825
syedtehseen <94910542+syedtehseen@users.noreply.github.com>
2022-09-11 21:05:16 +0530

    testing action inputs

--- ony-org.sh
+++ ony-org.sh
@@ -2,15 +2,18 @@
 #!/bin/bash/
 
 ECR_PREFIX="498031324058.dkr.ecr.us-east-1.amazonaws.com"
-DOCKERFILE_LIST="dockerfile-with-incorrect-ecr.txt"
+DOCKERFILE_LIST="incorrect-ecr-docker-files.txt"
+
+env
 
 > ${DOCKERFILE_LIST}
 
 for path in $(find . -iname "*dockerfile*" -exec ls -l {} \;  |  awk '{print $NF}')
 do
   ecr=$(grep -i FROM ${path} | awk '{print $2}' | awk -F"/" '{print $1}')
+
   if [[ ${ecr} != ${ECR_PREFIX} ]] ; then
-    echo "WARN: ${path} doesn't organization specific ECR address"
+    echo "WARN: ${path} dockerfile doesn't contain organization specific ECR address"
     echo ${path} >> ./${DOCKERFILE_LIST}
   else
     echo "INFO: You can merge your PR against your base branch"
@@ -19,5 +22,6 @@ done
 
 if [[ -s ${DOCKERFILE_LIST} ]]; then
   echo "ERROR: Following docker file(s) doesn't contain organization certified ECR address."
+  cat ${DOCKERFILE_LIST}
   exit 1
 fi
