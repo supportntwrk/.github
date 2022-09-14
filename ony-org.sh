7feef6b22bbeaa4f23b63399a7da245a0409696b
syedtehseen <94910542+syedtehseen@users.noreply.github.com>
2022-09-14 22:37:49 +0530

    debug

--- ony-org.sh
+++ ony-org.sh
@@ -1,9 +1,8 @@
-#set -x
 #!/bin/bash/
+set -x
 
 ECR_PREFIX="498031324058.dkr.ecr.us-east-1.amazonaws.com"
 DOCKERFILE_LIST="incorrect-ecr-docker-files.txt"
-
 
 > ${DOCKERFILE_LIST}
 
@@ -18,8 +17,10 @@ do
     echo "INFO: You can merge your PR against your base branch"
   fi
 done
+
 # opening file to see which docker files and under which branch it has been filtered
-cat ${DOCKERFILE_LIST}
+cat incorrect-ecr-docker-files.txt
+
 if [[ -s ${DOCKERFILE_LIST} ]]; then
   echo "ERROR: Following docker file(s) doesn't contain organization certified ECR address."
   cat ${DOCKERFILE_LIST}
